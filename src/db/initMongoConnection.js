f1f87fd7b1623fea8b36b4cb1feaf1a8b8921fe1
Nekittt <nikitaishchenko334@gmail.com>
2025-02-25 19:44:29 +0200

    Update dev function and add log database pre-connect

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -12,6 +12,7 @@ export async function initMongoConnection() {
     await mongoose.connect(
       `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
     );
+    console.log('Database pre-connected');
   } catch (e) {
     console.log('Error while setting up mongo connection', e);
   }
