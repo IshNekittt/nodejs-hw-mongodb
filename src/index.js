825d6cd417313cf1a9c7fb574c5fb5c8cd7ec245
Nekittt <nikitaishchenko334@gmail.com>
2025-02-25 18:15:35 +0200

    Add connection to the database

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
-setupServer();
+const bootstrap = async () => {
+  try {
+    await initMongoConnection();
+    setupServer();
+  } catch (e) {
+    console.log('Error during setup', e);
+  }
+};
+
+bootstrap();
