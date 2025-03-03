6cecd87d68069e96801d956819c304aa70d1924b
Nekittt <nikitaishchenko334@gmail.com>
2025-03-03 15:36:43 +0200

    Change structure of the project. Add controllers and router

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import 'dotenv/config';
 
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
 
 export function setupServer() {
   const PORT = Number(getEnvVar('PORT', '3000'));
@@ -12,6 +12,7 @@ export function setupServer() {
   const app = express();
 
   app.use(cors());
+  app.use(express.json());
 
   app.use(
     pino({
@@ -21,30 +22,7 @@ export function setupServer() {
     }),
   );
 
-  app.get('/contacts', async (_, res) => {
-    const data = await getAllContacts();
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: data,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      return res.status(404).json({ message: 'Contact not found' });
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
+  app.use(contactsRouter);
 
   app.use('*', (_, res) => {
     res.status(404).json({ message: 'Not found' });
